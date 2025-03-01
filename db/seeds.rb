682b333d3ef31a6d82596317c544e7e7ea4a82a6
pbagri@academic.rrc.ca <prince@LAPTOP-6BF4SRQD>
2025-02-28 20:03:54 -0600

    Added Faker gem and seeded database with products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+Product.destroy_all
+
+676.times do
+  Product.create(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence,
+    price: Faker::Commerce.price(range: 5.0..100.0),
+    stock_quantity: Faker::Number.between(from: 1, to: 50)
+  )
+end
