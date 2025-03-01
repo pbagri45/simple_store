08e171a4c5c4b7c6df38da7bb87a01869a2c01ea
pbagri@academic.rrc.ca <prince@LAPTOP-6BF4SRQD>
2025-02-28 20:00:30 -0600

    Added validations to Product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_01_015643) do
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.integer "stock_quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
