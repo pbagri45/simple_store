c446a9ab271a59fe825cee94e435f4bdd55099a3
pbagri@academic.rrc.ca <prince@LAPTOP-6BF4SRQD>
2025-02-28 20:09:07 -0600

    Added product index view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  get "products/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define resourceful routes for products (index and show)
+  resources :products, only: [:index, :show]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Define a health check route
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Set the root path to the products index page
+  root "products#index"
 end
